15353cebe9215c74f4df21e1e75864e57be60bce
Usman <usmanrdx903@gmail.com>
2026-01-15 19:51:48 +0500

    add testimonial slider

--- javscript/apps.js
+++ javscript/apps.js
@@ -1,1 +1,27 @@
 console.log("ready")
+
+// ---------------------------------------------TESTIMONIAL SLIDER-------------------------------------//
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    let testimonailSlider = new Swiper(".testimonailSlider", {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        initialSlide: 3.5,
+        centeredSlides: false,
+        autoplay: true,
+        loop: true,
+         pagination: {
+            el: ".testimonial-pagination",
+            dynamicBullets: true,
+        },
+        // grabCursor: true,
+        breakpoints: {
+            0: { slidesPerView: 1, spaceBetween: 1 },
+            768: { slidesPerView: 2, spaceBetween: 10 },
+            1024: { slidesPerView: 2.5, spaceBetween: 10 },
+            1300: { slidesPerView: 2.4, spaceBetween: 20 }
+        }
+       
+    });
+});
